5e8e05447f2f9ed5fcd5c49863a24e4df4274ccd
borislav <borislav.tchobanov@wacom.com>
2019-01-09 12:50:22 +0200

    successful emulator test "Yo, Manfred"

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
             return true;
         }
 
